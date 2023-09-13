0940aebd7d7002880132df2976e0c8a68735d4d3
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-13 15:05:00 +0200

    markup di base
    creato scheletro della to do list tramite html e bootstrap

--- script.js
+++ script.js
@@ -2,11 +2,35 @@
 
 const {createApp} = Vue
 
-createApp ({
+createApp({
     data() {
         return {
-            message:'ciao'
+            message: 'ciao',
+            tasks: [
+                {
+                    text: 'Fare spesa',
+                    done: true
+                },
+                {
+                    text: 'Riparare lavatrice',
+                    done: false
+                },
+                {
+                    text: 'Pagare bollette',
+                    done: true
+                }
+            ]
         }
     }
 }).mount('#app');
 
+
+
+
+
+/*
+- input -> v-model per leggere e scrivere
+- list item -> v-if
+- p -> else
+- button -> @click
+*/
