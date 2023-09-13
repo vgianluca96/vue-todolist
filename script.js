934453da5678ca2f9730d81f6e1916a035567920
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-13 15:55:11 +0200

    lista task con rimozione
    creata logica per creazione lista e rimozione elementi

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const {createApp} = Vue
 createApp({
     data() {
         return {
-            message: 'ciao',
+            condition: true,
             tasks: [
                 {
                     text: 'Fare spesa',
@@ -17,9 +17,20 @@ createApp({
                 },
                 {
                     text: 'Pagare bollette',
+                    done: false
+                },
+                {
+                    text: 'Lavare macchina',
                     done: true
                 }
             ]
+        }
+    },
+    methods: {
+        removeTask(index) {
+            this.tasks.splice(index,1);
+            console.log(index)
+            console.log(this.tasks)
         }
     }
 }).mount('#app');
